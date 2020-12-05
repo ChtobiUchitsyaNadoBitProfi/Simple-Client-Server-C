f3fbdf6f15b70cc6de7cc5579fbb018144504907
ChtobiUchitsyaNadoBitProfi <dindonich@pop-os.localdomain>
2020-12-06 01:46:07 +0700

    add my liteSH functions

---
+++ liteSH.h
@@ -0,0 +1,16 @@
+#ifndef LITESH_H
+#define LITESH_H
+
+#define SHELL "/bin/sh"
+
+int create_process(const char *command);
+
+int create_background_process(const char *command); 
+
+int send_signal(pid_t pid, int signum); 
+
+void signal_handler(int signumber); 
+
+int receive_signal(int signumber); 
+
+#endif
